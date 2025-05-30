3409ed3518f7675582fc45fc9449306464affe56
motylek25 <mattew.zakharov.03@mail.ru>
2025-05-30 12:10:47 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,4 +1,4 @@
-def find_min_subsequence_length(sequence):
+def min_subsequence_length(sequence):
     last_pos = {}
     min_length = float('inf')
     for idx, num in enumerate(sequence):
@@ -10,11 +10,10 @@ def find_min_subsequence_length(sequence):
                     min_length = current_length
     
     return min_length if min_length != float('inf') else "NONE"
-
 with open('data_prog_contest_problem_2.txt', 'r') as file:
     lines = file.readlines()
     n = int(lines[0].split()[0])
     sequence = list(map(int, lines[1].split()))
 
-result = find_min_subsequence_length(sequence)
+result = min_subsequence_length(sequence)
 print(result)
